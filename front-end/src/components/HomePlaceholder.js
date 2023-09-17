12514fb49451152624d786e1261148eeca978203
Eric Liu <ericliujt@gmail.com>
2023-09-17 05:28:12 -0400

    uncommented front page blobs

--- front-end/src/components/HomePlaceholder.js
+++ front-end/src/components/HomePlaceholder.js
@@ -28,7 +28,7 @@ function HomePlaceholder() {
       }} />
       
       {/* example questions blobs */}
-      {/* <Flex style={{
+      <Flex style={{
         backgroundColor: "#683F19",
         color: "#FFF5DF",
         padding: "28px",
@@ -55,7 +55,7 @@ function HomePlaceholder() {
       }} gap={4}  >
         <Text fontWeight="bold" fontSize="xl">Q2</Text>
         <Text fontSize="md">Describe the difference between the habits of various attachment styles.</Text>
-      </Flex> */}
+      </Flex>
 
     </Flex>
   );
