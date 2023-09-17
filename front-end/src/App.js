0d33852a3e054af145804f657f7bb2b1e374e48f
Udy Ding <udy.ding@gmail.com>
2023-09-16 22:49:05 -0400

    add chatbot conversation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,45 +1,61 @@
-import Uploader from './components/Uploader.js';
+import Uploader from "./components/Uploader.js";
 import Goals from "./components/Goals/Goals.js";
 import HomePlaceholder from "./components/HomePlaceholder.js";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import '@fontsource/manrope'
+import "@fontsource/manrope";
 
-import React, {useState} from 'react';
-import Chat from './Chat.js';
+import React, { useState, useEffect } from "react";
+import Chat from "./Chat.js";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
+  const GoalGroups = ["Memorization", "Explanation", "Application", "Analysis"];
+  const [shouldRedirect, setShouldRedirect] = useState(true);
+  const [notes, setNotes] = useState("");
+  const exampleNotes =
+    "A distributed system is a system whose components are located on different networked computers, which communicate and coordinate their actions by passing messages to one another. Distributed computing is a field of computer science that studies distributed systems." +
+    "The components of a distributed system interact with one another in order to achieve a common goal. Three significant challenges of distributed systems are: maintaining concurrency of components, overcoming the lack of a global clock, and managing the independent failure of components. When a component of one system fails, the entire system does not fail.[3] Examples of distributed systems vary from SOA-based systems to massively multiplayer online games to peer-to-peer applications." +
+    "A computer program that runs within a distributed system is called a distributed program, and distributed programming is the process of writing such programs. There are many different types of implementations for the message passing mechanism, including pure HTTP, RPC-like connectors and message queues.[6]" +
+    "Distributed computing also refers to the use of distributed systems to solve computational problems. In distributed computing, a problem is divided into many tasks, each of which is solved by one or more computers, which communicate with each other via message passing.";
 
-const GoalGroups = ["Memorization", "Explanation", "Application", "Analysis"];
-const [shouldRedirect, setShouldRedirect] = useState(true);
+  useEffect(() => {
+    setNotes(exampleNotes);
+  }, []);
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     const shouldSubmitForm = true;
 
-    if(shouldSubmitForm) {
-        setShouldRedirect(true);
+    if (shouldSubmitForm) {
+      setShouldRedirect(true);
     } else {
-        alert('Select the options above before moving forward')
+      alert("Select the options above before moving forward");
     }
-};
+  };
 
-const theme = extendTheme({
-  fonts: {
-    heading: `'Manrope', sans-serif`,
-    body: `'Manrope', sans-serif`,
-  },
-})
+  const theme = extendTheme({
+    fonts: {
+      heading: `'Manrope', sans-serif`,
+      body: `'Manrope', sans-serif`,
+    },
+  });
 
-if(shouldRedirect) {
+  if (shouldRedirect) {
     return (
       <ChakraProvider theme={theme}>
-        <Chat/>
+        <Chat />
       </ChakraProvider>
-    )
-}
+    );
+  }
 
+  if (shouldRedirect) {
+    return (
+      <ChakraProvider>
+        <Chat notes={notes} />
+      </ChakraProvider>
+    );
+  }
 
   return (
     <ChakraProvider theme={theme}>
@@ -48,10 +64,8 @@ if(shouldRedirect) {
       <Goals />
       <div>
         <form onSubmit={handleSubmit}>
-            <input 
-                type="text"
-            />
-            <Button type="submit">Start Teaching</Button>
+          <input type="text" />
+          <Button type="submit">Start Teaching</Button>
         </form>
       </div>
     </ChakraProvider>
--- front-end/src/Chat.js
+++ front-end/src/Chat.js
@@ -1,12 +1,100 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Heading } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 
-function Chat() {
+import { generateQuestions } from "./chatFunctions/generateQuestions.js";
+import { getResponse } from "./chatFunctions/getResponse.js";
+
+export function Chat({ notes }) {
+  const [chatHistory, setChatHistory] = useState([]);
+  const [getFollowupQuestion, setGetFollowupQuestion] = useState(true);
+  const [questions, setQuestions] = useState([]);
+  const [currQuestion, setCurrQuestion] = useState(1);
+  const [currentInput, setCurrentInput] = useState("");
+
+  useEffect(() => {
+    async function getQuestions() {
+      try {
+        // generate chat questions
+        const generatedQuestions = await generateQuestions(notes);
+        setQuestions(generatedQuestions);
+        setChatHistory([
+          ...chatHistory,
+          { user_name: "Chatbot", text: generatedQuestions[0] },
+        ]);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getQuestions();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const botResponse = await getResponse(currentInput, getFollowupQuestion, [
+      ...chatHistory,
+      { user_name: "User", text: currentInput },
+    ]);
+    const newResponses = [
+      { user_name: "User", text: currentInput },
+      { user_name: "Chatbot", text: botResponse },
+    ];
+
+    if (!getFollowupQuestion) {
+      // move on to next question
+      newResponses.push({
+        user_name: "Chatbot",
+        text: questions[currQuestion],
+      });
+    }
+
+    setChatHistory([...chatHistory, ...newResponses]);
+    setCurrQuestion(currQuestion + 1);
+    setGetFollowupQuestion(!getFollowupQuestion);
+  };
+
+  const currentText = chatHistory.at(-1);
+
   return (
     <main>
       <Heading size="md">Chat</Heading>
+      <div>
+        <p>old</p>
+        {chatHistory.slice(0, -1).map((entry) => {
+          return (
+            <h1>
+              {entry.user_name} {entry.text}
+            </h1>
+          );
+        })}
+        {currentText ? (
+          <>
+            <h1>CURRENT</h1>
+            <h3>
+              {currentText.user_name} : {currentText.text}
+            </h3>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      {currentText ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={currentInput}
+            style={{ background: "red" }}
+            onChange={(e) => setCurrentInput(e.target.value)}
+            disabled={currentText.user_name === "User"}
+          />
+          <button type="submit" disabled={currentText.user_name === "User"}>
+            Submit
+          </button>
+        </form>
+      ) : (
+        <></>
+      )}
     </main>
   );
 }
