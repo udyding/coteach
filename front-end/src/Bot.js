da24a26a68e6f300549fc6a0ecfa4cb75e21ea13
Bonnie Chin <b22chin@uwaterloo.ca>
2023-09-16 21:03:33 -0400

    bot component

---
+++ front-end/src/Bot.js
@@ -0,0 +1,20 @@
+import { Flex, Image, Tag, Text } from "@chakra-ui/react";
+import franklin from './assets/franklin.png'
+import "./styles/bot.css"
+
+
+export const Bot = ({ text }) => {
+    return (
+        <>
+            <Flex gap={6}>
+                <Flex flexDirection="column" gap={2}>
+                    <Image src={franklin} htmlWidth="90" fit alt='franklin' />
+                    <Tag size='md' fontWeight="bold" justifyContent='center' variant='subtle' colorScheme='orange'>Franklin</Tag>
+                </Flex>
+                <div className="typewriter">
+                    <Text fontSize="3xl" color="orange.500">{text}</Text>
+                </div>
+            </Flex>
+        </>
+    )
+}
